5711fcf9628017ecda41975ea016449f86e07304
Diego Nieto (lesandie) <lesandie@gmail.com>
2024-05-14 16:49:58 +0200

    WIP: serialization errors should do type conversion

--- scripts/test_ch_connect_asyncio.py
+++ scripts/test_ch_connect_asyncio.py
@@ -1,26 +1,40 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+from datetime import time
 import clickhouse_connect
 
-# Function to execute a query using clickhouse-connect synchronously
+# dictionary for passing settings to ClickHouse
+SETTINGS = {"session_id": "random-" + "-" + f"{time()}"}
+
+
 def execute_query_sync(query):
-    client = clickhouse_connect.get_client(host='localhost', port='8123', user='default')
-    result = client.query(query)
+    """
+    Function to execute a query using clickhouse-connect synchronously
+    """
+    client = clickhouse_connect.get_client(
+        host="localhost", port="8123", user="default"
+    )
+    result = client.query(query, settings=SETTINGS)
     return result
 
-# Asynchronous wrapper function to run the synchronous function in a thread pool
+
 async def execute_query_async(query):
+    """
+    Asynchronous wrapper function to run the synchronous function in a thread pool
+    """
     loop = asyncio.get_running_loop()
     # Use ThreadPoolExecutor to execute the synchronous function
     with ThreadPoolExecutor() as pool:
         result = await loop.run_in_executor(pool, execute_query_sync, query)
         return result
 
+
 async def main():
-    query = "SELECT * FROM system.query_log LIMIT 10"  # Example query
+    query = "SELECT * FROM system.query_log ORDER BY event_time DESC LIMIT 10"  # Example query
     result = await execute_query_async(query)
     print(result.first_row)
 
+
 # Run the async main function
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
---
+++ scripts/test_ch_connect_asyncio_insert.py
@@ -0,0 +1,108 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from datetime import time
+import clickhouse_connect
+import csv
+
+# environment variables
+HOST = "localhost"
+PORT = 8123
+USER = "default"
+PASSWORD = ""
+# dictionary for passing settings to ClickHouse
+SETTINGS = {"session_id": "random-" + "-" + f"{time()}"}
+
+
+def execute_query_sync(client, query):
+    """
+    Function to execute a query using clickhouse-connect synchronously
+    """
+    result = client.query(query, settings=SETTINGS)
+    return result
+
+
+def execute_insert_sync(client):
+    """
+    Function to execute an insert using clickhouse-connect synchronously:
+    https://github.com/ClickHouse/clickhouse-connect/blob/457533df05fa685b2a1424359bea5654240ef971/examples/insert_examples.py
+    """
+
+    # Define the CSV file path
+    csv_file_path = "file.csv"
+
+    # Open the CSV file and read data
+    with open(csv_file_path, mode="r") as file:
+        csv_reader = csv.DictReader(file)
+        data = [row for row in csv_reader]
+
+    prepared_data = []
+    for row in data:
+        prepared_data.append(list(row.values()))
+
+    # Insert the data into the ClickHouse table ... do some batching
+    client.insert(
+        database="default",
+        table="insert_test",
+        data=prepared_data,
+        column_names=[
+            "id_order",
+            "id_plat",
+            "id_warehouse",
+            "id_product",
+            "order_type",
+            "order_status",
+            "datetime_order",
+            "units",
+            "total",
+        ],
+        column_type_names=[
+            "UInt16",
+            "UInt32",
+            "UInt64",
+            "UInt16",
+            "UInt16",
+            "String",
+            "DateTime",
+            "Int16",
+            "Float32",
+        ],
+        settings=SETTINGS,
+    )
+
+
+async def execute_query_async(client, query):
+    """
+    Asynchronous wrapper function to run the synchronous function in a thread pool
+    """
+    loop = asyncio.get_running_loop()
+    # Use ThreadPoolExecutor to execute the synchronous function
+    with ThreadPoolExecutor() as pool:
+        result = await loop.run_in_executor(pool, execute_query_sync, client, query)
+        return result
+
+
+async def execute_insert_async(client):
+    """
+    Asynchronous wrapper function to run the synchronous function in a thread pool
+    """
+    loop = asyncio.get_running_loop()
+    # Use ThreadPoolExecutor to execute the synchronous function
+    with ThreadPoolExecutor() as pool:
+        result = await loop.run_in_executor(pool, execute_insert_sync, client)
+        return result
+
+
+async def main():
+    client = clickhouse_connect.get_client(
+        host=HOST, port=PORT, user=USER, password=PASSWORD, settings=SETTINGS
+    )
+    query = "SELECT * FROM system.query_log ORDER BY event_time DESC LIMIT 10"  # Example query
+    result = await execute_query_async(client, query)
+    print(result.first_row)
+    result_insert = await execute_insert_async(client)
+    print(result_insert)
+
+
+# Run the async main function
+if __name__ == "__main__":
+    asyncio.run(main())
